f4abbd11042d23133e63d010f81f11c3b09bf8c8
Anthony Garcia <anthonydeyvis32@gmail.com>
2022-04-23 23:14:59 -0400

    first endpoint, welcome

---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
+	"github.com/th3khan/rest-web-sockets-with-go/handlers"
+	"github.com/th3khan/rest-web-sockets-with-go/server"
+)
+
+func main() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	PORT := os.Getenv("PORT")
+	JWT_SECRET := os.Getenv("JWT_SECRET")
+	DATABASE_URL := os.Getenv("DATABASE_URL")
+
+	s, err := server.NewServer(context.Background(), &server.Config{
+		Port:        PORT,
+		JWTSecret:   JWT_SECRET,
+		DataBaseUrl: DATABASE_URL,
+	})
+
+	if err != nil {
+		log.Fatal("Error", err)
+	}
+	s.Start(BindRouter)
+}
+
+func BindRouter(s server.Server, r *mux.Router) {
+	r.HandleFunc("/", handlers.HomeHandler(s)).Methods(http.MethodGet)
+}
