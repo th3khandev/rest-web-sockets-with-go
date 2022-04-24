3a9091077ce0313f882b19f5cc3897312bc1624c
Anthony Garcia <anthonydeyvis32@gmail.com>
2022-04-23 22:50:53 -0400

    defined server and broker

---
+++ server/server.go
@@ -0,0 +1,56 @@
+package server
+
+import (
+	"context"
+	"errors"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+type Config struct {
+	Port        string
+	JWTSecret   string
+	DataBaseUrl string
+}
+
+type Server interface {
+	Config() *Config
+}
+
+type Broker struct {
+	config *Config
+	router *mux.Router
+}
+
+func (b *Broker) Config() *Config {
+	return b.config
+}
+
+func NewServer(ctx context.Context, config *Config) (*Broker, error) {
+	if config.Port == "" {
+		return nil, errors.New("Port is required")
+	}
+	if config.JWTSecret == "" {
+		return nil, errors.New("Secret Key is Required")
+	}
+	if config.DataBaseUrl == "" {
+		return nil, errors.New("Database url is required")
+	}
+	broker := &Broker{
+		config: config,
+		router: mux.NewRouter(),
+	}
+	return broker, nil
+}
+
+func (b *Broker) Start(binder func(s Server, r *mux.Router)) {
+	b.router = mux.NewRouter()
+	binder(b, b.router)
+	log.Println("Starting server on Port", b.config.Port)
+
+	if err := http.ListenAndServe(b.config.Port, b.router); err != nil {
+		log.Fatal("ListenAndServer: ", err)
+	}
+}
