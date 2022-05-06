4013e651aee974c60bfe92d0ea9a21d32c521d5f
Anthony Garcia <anthonydeyvis32@gmail.com>
2022-05-06 13:23:32 -0400

    create post table

--- database/up.sql
+++ database/up.sql
@@ -6,3 +6,15 @@ CREATE TABLE users (
     password VARCHAR(255) NOT NULL,
     created_at TIMESTAMP NOT NULL DEFAULT NOW()
 );
+
+DROP TABLE IF EXISTS posts;
+
+CREATE TABLE posts (
+    id VARCHAR(50) PRIMARY KEY,
+    user_id VARCHAR(50) NOT NULL,
+    title VARCHAR(255) NOT NULL,
+    content TEXT NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    INDEX (user_id),
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
