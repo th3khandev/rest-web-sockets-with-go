a1cd601bf47ac38b11b69f74cb04f0ed25563287
Anthony Garcia <anthonydeyvis32@gmail.com>
2022-05-05 21:28:30 -0400

    add close function in user repository

--- repositories/user.go
+++ repositories/user.go
@@ -9,6 +9,7 @@ import (
 type UserRepository interface {
 	InsertUser(ctx context.Context, user *models.User) error
 	GetUserById(ctx context.Context, id int64) (*models.User, error)
+	Close() error
 }
 
 var implementation UserRepository
@@ -23,4 +24,8 @@ func InsertUser(ctx context.Context, user *models.User) error {
 
 func GetUserById(ctx context.Context, id int64) (*models.User, error) {
 	return implementation.GetUserById(ctx, id)
+}
+
+func Close() error {
+	return implementation.Close()
 }
