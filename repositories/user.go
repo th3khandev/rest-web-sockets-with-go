e28ae54e3d301a4571c2d2b8eac82ead046f4a71
Anthony Garcia <anthonydeyvis32@gmail.com>
2022-05-05 20:32:40 -0400

    create user repository

---
+++ repositories/user.go
@@ -0,0 +1,26 @@
+package repositories
+
+import (
+	"context"
+
+	"github.com/th3khan/rest-web-sockets-with-go/models"
+)
+
+type UserRepository interface {
+	InsertUser(ctx context.Context, user *models.User) error
+	GetUserById(ctx context.Context, id int64) (*models.User, error)
+}
+
+var implementation UserRepository
+
+func SetRepository(repository UserRepository) {
+	implementation = repository
+}
+
+func InsertUser(ctx context.Context, user *models.User) error {
+	return implementation.InsertUser(ctx, user)
+}
+
+func GetUserById(ctx context.Context, id int64) (*models.User, error) {
+	return implementation.GetUserById(ctx, id)
+}
